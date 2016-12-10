0991ef175afd076ef57a2925428415ae419657ee
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-12-10 16:54:22 -0330

    Use CRC16-CCITT

--- sdk/lib/Checksum.ts
+++ sdk/lib/Checksum.ts
@@ -1,6 +1,6 @@
 const crc = require(`crc`);
 const crc16 = (buffer: Buffer): Buffer => {
-    const result = crc.crc16(buffer);
+    const result = crc.crc16ccitt(buffer);
     return Buffer.from([result >> 8, result]);
 };
 
