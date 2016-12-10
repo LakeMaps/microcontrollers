1c5b99ef6f6b2fe0d4c57676a3d96e81589310b1
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-12-10 20:27:20 -0330

    Fix SerialPort#read timing issue

--- sdk/lib/serial/SerialPort.ts
+++ sdk/lib/serial/SerialPort.ts
@@ -9,23 +9,35 @@ export class SerialPort {
         this.port = new NativeSerialPort(port, {
             baudrate: 9600,
         });
-        this.port.on(`data`, (data?: Buffer) => {
-            if (data === undefined) {
-                return;
-            }
-            this.buffer = Buffer.concat([this.buffer, data]);
-        });
     }
 
-    read(): (n: number) => Promise<Buffer> {
-        return (n: number) => new Promise((resolve) => {
+    onData(n: number, callback: (b: Buffer) => void, data?: Buffer) {
+        if (data === undefined) {
+            return;
+        }
+
+        this.buffer = Buffer.concat([this.buffer, data]);
+        if (this.buffer.length < n) {
+            return;
+        }
+        if (this.buffer.length == n) {
+            const result = Buffer.alloc(n);
+            this.buffer.copy(result, 0, 0, n);
+            this.buffer = Buffer.alloc(0);
+            callback(result);
+        }
+        if (this.buffer.length > n) {
             const result = Buffer.alloc(n);
             const leftovers = Buffer.alloc(this.buffer.length - n);
             this.buffer.copy(result, 0, 0, n);
             this.buffer.copy(leftovers, 0, n);
             this.buffer = leftovers;
-            resolve(result);
-        });
+            callback(result);
+        }
+    }
+
+    read(): (n: number) => Promise<Buffer> {
+        return (n: number) => new Promise((resolve) => this.port.on(`data`, this.onData.bind(this, n, resolve)));
     }
 
     write(): (buffer: Buffer) => Promise<number> {
