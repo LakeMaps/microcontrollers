8c715f819313b00af59930fdabfac79a829ba833
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2016-12-17 14:57:42 -0330

    WIP

--- sdk/lib/serial/SerialPort.ts
+++ sdk/lib/serial/SerialPort.ts
@@ -27,6 +27,12 @@ export class SerialPort {
         this.buffer = Buffer.concat([this.buffer, data]);
         for (let i = 0; i < this.pendingReads.length; i++) {
             const nextRead = this.pendingReads[i];
+            if (
+                   (this.buffer[0] == 0xAA)
+                && (this.buffer[1] == 0x0F || this.buffer[1] == 0x1F)
+            ) {
+                this.buffer = this.buffer.slice(5);
+            }
             if (this.buffer.length >= nextRead.n) {
                 const result = Buffer.alloc(nextRead.n);
                 const leftovers = Buffer.alloc(this.buffer.length - nextRead.n);
